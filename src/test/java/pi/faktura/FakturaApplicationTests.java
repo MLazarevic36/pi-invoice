07c5fbef069915cc3477e268c1c785878ddb04c2
MLazarevic36 <hrle96ab@gmail.com>
2020-03-04 13:04:37 +0100

    Initial commit

---
+++ src/test/java/pi/faktura/FakturaApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.faktura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
